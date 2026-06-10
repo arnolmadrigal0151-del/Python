90642568f371432ff0beab9a7ac8512261f60e86
ArnolME <arnol.madrigal0151@alumnos.udg.mx>
2026-06-10 14:06:57 -0600

    agregue una cuarta linea

--- hola.py
+++ hola.py
@@ -1,3 +1,4 @@
 print("todo listo y configurado con la IA!")
 print("¡Este es mi segundo avance del curso!")
 print("¡Tercera línea: Git y GitHub dominados al 100%!")
+print ("¡Cuarta línea: Estoy listo para conquistar el mundo de la programación con IA!" )
